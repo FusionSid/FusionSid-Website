73ae18019be48a189adf0d0571397c8886e5e3d7
FusionSid <siddheshadsv@icloud.com>
2023-01-25 15:49:52 +1300

    moyai assets

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,12 @@
 import HomeText from "./hometext"
+import { Moyai } from "../assets"
+
 
 export default function Hero() {
     return (
         <>
             <HomeText />
+            <img src={Moyai} className="w-52 absolute right-24 bottom-20"></img>
         </>
     )
 }
