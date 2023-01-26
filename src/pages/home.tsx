6f18c48741a0ad3921a09f6a8c816d01c68ba01d
FusionSid <siddheshadsv@icloud.com>
2023-01-27 10:14:50 +1300

    chore: update components to use the useEffect hook for event listners

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,26 +1,17 @@
 import { Hero } from "../components";
 
-import { useEffect } from "react";
-
 export default function Home() {
 	let keylog: Array<string> = [];
-	const command = "rm -rf /";
-
-	useEffect(() => {
-		// close the navbar when someone hits the esc key
-		const keyHandler = (event: KeyboardEvent) => {
-			keylog.push(event.key);
-			if (keylog.join("").includes(command)) {
-				setInterval(() => (document.body.className = "hidden"), 5000);
-				document.body.className = "animate-fade";
-				document.removeEventListener("keydown", keyHandler);
-			}
-		};
-		document.addEventListener("keydown", keyHandler);
-		return () => {
+	const keyHandler = (event: KeyboardEvent) => {
+		const command = "rm -rf /";
+		keylog.push(event.key);
+		if (keylog.join("").includes(command)) {
+			setInterval(() => (document.body.className = "hidden"), 5000);
+			document.body.className = "animate-fade";
 			document.removeEventListener("keydown", keyHandler);
-		};
-	});
+		}
+	};
+	document.addEventListener("keydown", keyHandler);
 
 	return (
 		<div className="antialiased transition-colors duration-1000 bg-bglight dark:bg-bgdark">
