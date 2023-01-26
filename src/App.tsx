9755317bc2d8512dcf212464c49fca4d6a7bd26f
FusionSid <siddheshadsv@icloud.com>
2023-01-26 21:05:49 +1300

    feat: auto add nav bar and footer to each page

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,53 @@
+// hooks
+import { useState, useEffect } from "react";
+
 // import router
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // import pages
 import * as pages from "./pages";
 
+// import footer and navbar
+import { Navbar, Footer } from "./components";
+
+// dark mode
+import { NavbarLinks, isDarkMode } from "./constants";
+
 // import base css
 import "./App.css";
 
 function App() {
-    return (
-        <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<pages.Home />}></Route>
-                    <Route path="/about" element={<pages.About />}></Route>
-                    <Route
-                        path="/projects"
-                        element={<pages.Projects />}
-                    ></Route>
-                    <Route path="/contact" element={<pages.Contact />}></Route>
-                    <Route
-                        path="/terminal"
-                        element={<pages.Terminal />}
-                    ></Route>
-                    <Route path="/*" element={<pages.NotFound404 />}></Route>
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
+	const [darkMode, setDarkMode] = useState(isDarkMode());
+
+	useEffect(() => {
+		const darkModeUpdateHandler = (event: StorageEvent) => {
+			setDarkMode(localStorage.theme);
+		};
+		window.addEventListener("storage", darkModeUpdateHandler);
+	});
+
+	return (
+		<div className={`${darkMode === "dark" ? "dark " : ""}`}>
+			<BrowserRouter>
+				<Navbar navlinks={NavbarLinks} />
+				<Routes>
+					<Route path="/" element={<pages.Home />}></Route>
+					<Route path="/about" element={<pages.About />}></Route>
+					<Route
+						path="/projects"
+						element={<pages.Projects />}
+					></Route>
+					<Route path="/contact" element={<pages.Contact />}></Route>
+					<Route
+						path="/terminal"
+						element={<pages.Terminal />}
+					></Route>
+					<Route path="/*" element={<pages.NotFound404 />}></Route>
+				</Routes>
+				<Footer />
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,3 @@
 export default function Footer() {
-    return <></>;
+    return <div className="bg-bglight dark:bg-bgdark transition-colors duration-1000">Footer placeholder eeeeee</div>;
 }
