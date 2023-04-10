07268b898fc80f297b8e388afff36b1536829a22
FusionSid <siddheshadsv@icloud.com>
2023-04-11 00:16:44 +1200

    feat: redesign skills section to have animation and tilt on hover

--- src/components/homepage/skills_section.tsx
+++ src/components/homepage/skills_section.tsx
@@ -1,43 +1,65 @@
-import { techLogoImages, languageLogoImages } from "../../utils";
-
+import { technoligies } from "../../utils";
+import Tilt from "react-parallax-tilt";
+import WhenInView from "../inview";
+import { motion } from "framer-motion";
 export default function SkillsSection() {
 	const skillsHeader =
 		"dark:text-white text-[1.5rem] sm:text-[2rem] font-jetbrains";
-	const languageStyle = "w-[25%] md-[20%] rounded mx-auto my-5";
-	const technologiesStyle = "w-[25%] sm:w-[50%] rounded mx-auto my-5";
+	const technologiesStyle = "w-[25%] sm:w-[45%] rounded mx-auto my-10";
 
 	return (
-		<div className="m-5 lg:border-b-[0.1px] border-opacity-10 border-white z-10">
-			<h1 className={skillsHeader}>{"<skills>"}</h1>
-			<div className="flex justify-center lg:flex-row flex-col">
-				<div className="lg:border-r-[0.1px] border-opacity-10 border-white w-full">
-					<h1 className="dark:text-white text-[2rem] sm:text-[3rem] font-poppins font-bold text-center m-3">
-						Languages
-					</h1>
-					<div className="grid grid-cols-2">
-						{languageLogoImages.map((value) => (
-							<img className={languageStyle} src={value} alt="" />
-						))}
-					</div>
-				</div>
-				<div className="lg:border-l-[0.1px] border-opacity-10 border-white w-full">
-					<h1 className="dark:text-white text-[2rem] sm:text-[3rem] font-poppins font-bold text-center m-3">
-						Technologies
-					</h1>
-					<div className="container mx-auto">
-						<div className="grid grid-cols-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-							{techLogoImages.map((value) => (
-								<img
-									className={technologiesStyle}
-									src={value}
-									alt=""
-								/>
-							))}
+		<WhenInView
+			div={
+				<div className="z-10 m-5 border-white border-opacity-10 lg:border-b-[0.1px]">
+					<h1 className={skillsHeader}>{"<skills>"}</h1>
+					<div className="flex flex-col justify-center lg:flex-row">
+						<div className="container mx-auto">
+							<div className="my-10 grid grid-cols-2 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+								{technoligies
+									.map((value) => ({
+										value,
+										sort: Math.random(),
+									}))
+									.sort((a, b) => a.sort - b.sort)
+									.map(({ value }) => value)
+									.map(([name, image, link], index) => (
+										<Tilt>
+											<motion.div
+												variants={{
+													visible: {
+														opacity: 1,
+														y: 0,
+														transition: { duration: 0.55, ease: "easeInOut", delay: index*0.2},
+													},
+													hidden: { opacity: 0, y: 200 },
+												}}
+											>
+												<a
+													href={link}
+													target="_blank"
+													className="flex flex-col rounded-xl border-2 border-black border-opacity-10 pb-5 text-center uppercase shadow-xl hover:cursor-pointer dark:border-white dark:border-opacity-10 dark:text-[#BDEBEA]"
+												>
+													<img
+														className={
+															technologiesStyle
+														}
+														src={image}
+														alt={name}
+													/>
+													<h1>{name}</h1>
+												</a>
+											</motion.div>
+										</Tilt>
+									))}
+								<h1 className="dark:text-white">
+									(not in any particular order)
+								</h1>
+							</div>
 						</div>
 					</div>
+					<h1 className={skillsHeader}>{"</skills>"}</h1>
 				</div>
-			</div>
-			<h1 className={skillsHeader}>{"</skills>"}</h1>
-		</div>
+			}
+		/>
 	);
 }
