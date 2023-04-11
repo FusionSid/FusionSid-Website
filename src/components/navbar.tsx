5e4310c282580ccd06775cc52542d60e3613d1d9
FusionSid <siddheshadsv@icloud.com>
2023-04-12 01:32:45 +1200

    feat: add animation to logo on navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -71,28 +71,106 @@ export default function Navbar(props: NavbarProps) {
 					<motion.svg
 						animate={{
 							opacity: 1,
-							y: 0,
+							x: 0,
 							transition: {
 								duration: 0.45,
 								ease: "easeInOut",
+								delay: 1,
 							},
 						}}
 						initial={{
 							opacity: 0,
-							y: 100,
+							x: -100,
 						}}
 						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 1090.000000 2001.000000"
+						viewBox="0 0 347 620"
 						className="ml-10 mr-2 flex h-12 w-12 gap-3 hover:animate-spin-slow hover:cursor-pointer dark:fill-white lg:h-14 lg:w-14"
 						onClick={() => navigate("/")}
 					>
-						<g
-							transform="translate(0.000000,2001.000000) scale(0.100000,-0.100000)"
-							stroke="none"
-						>
-							<path d="M4953 19990 c-18 -10 -40 -31 -50 -47 -17 -26 -18 -183 -23 -3733 l-5 -3706 -1970 3555 c-1084 1955 -1982 3571 -1997 3592 -57 80 -194 131 -294 110 -66 -13 -371 -179 -421 -229 -72 -72 -106 -193 -80 -287 8 -30 974 -1783 2390 -4338 l2377 -4287 0 -683 0 -682 -2351 -4240 c-1293 -2332 -2367 -4274 -2387 -4315 -32 -65 -36 -84 -36 -146 1 -84 31 -158 86 -211 40 -38 347 -209 403 -224 98 -27 228 12 296 88 19 21 923 1642 2009 3602 l1975 3564 5 -3636 c5 -3635 5 -3636 25 -3663 11 -15 36 -35 55 -45 32 -18 65 -19 456 -19 420 0 421 0 454 22 18 13 43 36 54 51 l21 28 5 3632 5 3631 1978 -3569 c1681 -3034 1984 -3576 2023 -3612 76 -71 189 -100 288 -72 55 15 355 184 394 222 69 66 103 182 83 277 -8 38 -635 1177 -2391 4345 l-2380 4294 0 688 0 688 2401 4330 c1321 2382 2411 4350 2422 4375 15 32 21 66 21 116 0 60 -5 81 -32 137 -40 80 -63 99 -252 203 -210 116 -246 126 -350 100 -80 -21 -144 -66 -184 -130 -18 -28 -930 -1671 -2027 -3650 l-1994 -3599 -5 3696 -5 3696 -21 28 c-11 15 -36 38 -54 51 -33 22 -34 22 -459 22 -405 0 -428 -1 -458 -20z" />
+						<g>
+							<rect x="157.056" width="33" height="620" rx="7" />
+
+							<motion.rect
+								animate={{
+									height: 323.597,
+									transition: {
+										duration: 1.0,
+										ease: "easeInOut",
+										delay: 1.45,
+									},
+								}}
+								initial={{
+									height: 0,
+								}}
+								y="17.9004"
+								width="32.7254"
+								height="323.597"
+								rx="7"
+								transform="rotate(-30 0 17.9004)"
+							/>
+
+							<motion.rect
+								animate={{
+									height: 323.597,
+									transition: {
+										duration: 1.0,
+										ease: "easeInOut",
+										delay: 1.45,
+									},
+								}}
+								initial={{
+									height: 0,
+								}}
+								x="318.7"
+								y="1.53772"
+								width="32.7254"
+								height="323.597"
+								rx="7"
+								transform="rotate(30 318.7 1.53772)"
+							/>
+
+							<motion.rect
+								animate={{
+									height: 323.597,
+									transition: {
+										duration: 1.0,
+										ease: "easeInOut",
+										delay: 1.45,
+									},
+								}}
+								initial={{
+									height: 0,
+								}}
+								x="28.3975"
+								y="619.606"
+								width="32.7254"
+								height="323.597"
+								rx="7"
+								transform="rotate(-150 28.3975 619.606)"
+							/>
+
+							<motion.rect
+								animate={{
+									height: 323.597,
+									transition: {
+										duration: 1.0,
+										ease: "easeInOut",
+										delay: 1.45,
+									},
+								}}
+								initial={{
+									height: 0,
+								}}
+								x="347.097"
+								y="603.243"
+								width="32.7254"
+								height="323.597"
+								rx="7"
+								transform="rotate(150 347.097 603.243)"
+							/>
 						</g>
 					</motion.svg>
+
 					{ShowNavLogoText ? (
 						<motion.div
 							animate={{
