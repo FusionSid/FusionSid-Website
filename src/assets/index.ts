abd5b4786ac46ccc39836048140031e645c234fb
FusionSid <siddheshadsv@icloud.com>
2023-04-12 02:34:04 +1200

    chore: removing guy coding image

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,12 +2,10 @@ import LogoWhite from "./images/logowhite.png"
 import NavLogo from "./images/navlogo.png"
 import Moyai from "./images/moyai.png"
 import Moai from "./images/moai.png"
-import PersonCodingImage from './images/guycoding.svg'
 
 export {
     Moai,
     Moyai,
     NavLogo,
     LogoWhite,
-    PersonCodingImage
 }
