470162a0ebef7d8f7164f0fff2d85c98a8d0de8b
FusionSid <siddheshadsv@icloud.com>
2023-04-12 14:01:12 +1200

    feat: notif that page needs to still be done on other pages

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,3 +1,3 @@
 export default function About() {
-    return <></>;
+    return <>PAGE TODO</>;
 }
--- src/pages/terminal.tsx
+++ src/pages/terminal.tsx
@@ -1,3 +1,3 @@
 export default function Terminal() {
-    return <></>;
+    return <>PAGE TODO</>;
 }
